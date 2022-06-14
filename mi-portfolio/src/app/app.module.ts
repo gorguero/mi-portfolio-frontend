959aa0444823c27dc7f7967853cff2bdd246f242
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-06-13 22:08:40 -0300

    Cargando componentes V1
    Se han agregado los componentes de:
    -Componets en general
    -Encabezado
    -Acerca-de
    -Educacion y experiencia laboral

--- mi-portfolio/src/app/app.module.ts
+++ mi-portfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { MiPerfilComponent } from './components/acerca-de/mi-perfil/mi-perfil.component';
+import { SobreMiComponent } from './components/acerca-de/sobre-mi/sobre-mi.component';
+import { EducacionYExperienciaComponent } from './components/educacion-y-experiencia/educacion-y-experiencia.component';
+import { EducacionComponent } from './components/educacion-y-experiencia/educacion/educacion.component';
+import { ExperienciaLaboralComponent } from './components/educacion-y-experiencia/experiencia-laboral/experiencia-laboral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    MiPerfilComponent,
+    SobreMiComponent,
+    EducacionYExperienciaComponent,
+    EducacionComponent,
+    ExperienciaLaboralComponent
   ],
   imports: [
     BrowserModule,
