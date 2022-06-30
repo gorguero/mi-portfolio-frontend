bd62dfea1104ce607ae395e3cd771cdbf3f6d28a
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-06-30 16:25:15 -0300

    Creacion de componentes en admi
    Se creo:
    -Habilidades admi
    -Proyectos admi
    -Tecnologias admi

---
+++ mi-portfolio/src/app/administrador/habilidades-admi/habilidades-admi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habilidades-admi',
+  templateUrl: './habilidades-admi.component.html',
+  styleUrls: ['./habilidades-admi.component.css']
+})
+export class HabilidadesAdmiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ mi-portfolio/src/app/administrador/proyectos-admi/proyectos-admi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos-admi',
+  templateUrl: './proyectos-admi.component.html',
+  styleUrls: ['./proyectos-admi.component.css']
+})
+export class ProyectosAdmiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
