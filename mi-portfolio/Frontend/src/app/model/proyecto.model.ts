02d72d9bd1793084af21a5b33423954abac015ee
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-09-15 17:28:24 -0300

    Creando y conectando CRUD de Proyecto

--- mi-portfolio/Frontend/src/app/model/proyecto.model.ts
+++ mi-portfolio/Frontend/src/app/model/proyecto.model.ts
@@ -5,13 +5,14 @@ export class Proyecto{
     descripcion:String;
     link_portada:String;
     link_proyecto:String;
+    tecnologias:String;
 
-    constructor(id:number, titulo:String, descripcion:String, link_portada:String, link_proyecto:String){
-        this.id = id;
+    constructor(titulo:String, descripcion:String, link_portada:String, link_proyecto:String, tecnologias:String){
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.link_portada = link_portada;
         this.link_proyecto = link_proyecto;
+        this.tecnologias = tecnologias;
     }
 
 }
