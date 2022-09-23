d4ff0570477b223e2169c88a744951e0a05df628
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-09-23 11:57:58 -0300

    Conectando JWT con login

--- mi-portfolio/Frontend/src/app/components/encabezado/encabezado.component.ts
+++ mi-portfolio/Frontend/src/app/components/encabezado/encabezado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PorfolioService } from 'src/app/servicios/porfolio.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-encabezado',
@@ -10,12 +12,26 @@ export class EncabezadoComponent implements OnInit {
 
   miPortfolio:any;
 
-  constructor(private datosPorfolio:PorfolioService) { } //Inyectar servicio a un componente
+  isLogged = false;
+
+  constructor(private datosPorfolio:PorfolioService, private router:Router, private tokenService:TokenService) { } //Inyectar servicio a un componente
 
   ngOnInit(): void {
+
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
+
     this.datosPorfolio.obtenerDatos().subscribe(data => {
       this.miPortfolio = data;
     });
+  }
+
+  onLogOut():void{
+    this.tokenService.logOut();
+    window.location.reload();
   }
 
 }
